8e7d6a3169a4912903c7fcc737b377997764956b
Vladislav Zagoronyuk <vlad.v.zag@gmail.com>
2023-04-18 02:36:08 +0300

    MyHash adde

--- Data/LexParser.py
+++ Data/LexParser.py
@@ -3,10 +3,17 @@ import re
 with open("./input_text.txt", "r") as input_file:
     text = input_file.read()
 
-# print(text)
+parsed = re.findall(r'[A-Za-z]+', text)     # find all words and skip punctuation marks
+parsed = list(set(parsed))                  # remove repeated words from list
 
-parsed = re.findall(r'[A-Za-z]+', text)
-parsed_text = ' '.join(parsed)
+for i in range(len(parsed)):
+    parsed_i_byte = parsed[i].encode('utf-8')
+    print(parsed[i], end=": ")
+    for j in range(len(parsed[i])):
+        print(parsed_i_byte[j], end=" ")
+    print()
+
+parsed_text = ' '.join(parsed)              # convert from list to string
 
 with open("./input_text_parsed.txt", "w") as output_file:
     output_file.write(str(len(parsed)))
